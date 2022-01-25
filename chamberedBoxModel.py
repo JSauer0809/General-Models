ba27621924f2c4702b0e2d35cf9620fc4fd1ef25
JonnaySau <jsauer@ucsd.edu>
2022-01-25 15:30:31 -0500

    Update chamberedBoxModel.py

--- chamberedBoxModel.py
+++ chamberedBoxModel.py
@@ -1,10 +1,11 @@
+#This very simple box model describes a set of 5 chambers which exchange air based on the input flow rate in liters per minute. Box chambers are assumed to be well-mixed. 
 sectionWidth = 0.171 #width of segment (mm)
 sectionLength = 0.190 #length of segment (mm)
 sectionHeight = 0.050 #height of segment (mm)
 flowRateLPM = 0.5 #liter per min headspace flow
 flowRateLPs = flowRateLPM/60 #liters per second headspace flow
 flowRatems = flowRateLPs/1000 #cubic meters per second headspace flow
-time = 0
+time = 0 #s
 emissionRate = 0.1 #micrograms /s
 chamber1mass = 0
 c1conc = 0
@@ -17,11 +18,11 @@ c4conc = 0
 chamber5mass = 0
 c5conc = 0
 
-while time < 20000:
-    if time == 120:
+while time < 20000: #duration of time box model runs
+    if time == 120: #Sets a time in which emission of material into box1 ends
         emissionRate = 0
-    chamber1mass = chamber1mass + emissionRate -  (flowRatems*c1conc)
-    c1conc = chamber1mass/(sectionWidth*sectionLength*sectionHeight)
+    chamber1mass = chamber1mass + emissionRate -  (flowRatems*c1conc) #calculates mass in box1
+    c1conc = chamber1mass/(sectionWidth*sectionLength*sectionHeight) #calculates concentration in box1
                                   
     chamber2mass = chamber2mass + (flowRatems*c1conc) - (flowRatems*c2conc)
     c2conc = chamber2mass/(sectionWidth*sectionLength*sectionHeight)
@@ -37,7 +38,7 @@ while time < 20000:
     time = time+1
                                   
     #print(headspaceMass,time)
-    f = open("WBA_ChamberedBox_0_5LPM_EmissionStop2", "a")
+    f = open("WBA_ChamberedBox_0_5LPM_EmissionStop2", "a") #writes mass of material in each chamber to file separated by commas
     f.write(str(chamber1mass))
     f.write(",")
     f.write(str(chamber2mass))
